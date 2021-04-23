d99dce749b01e65e16fa0fd22ab1090c1601bbd7
Felix Pacheco <felix@servisenior.com>
2021-04-22 20:33:24 -0400

    Passing params on navigate

--- src/screens/todos/TodosList.tsx
+++ src/screens/todos/TodosList.tsx
@@ -1,5 +1,5 @@
 import {
-  FlatList, View, Pressable,
+  FlatList, View, Pressable, Text
 } from 'react-native';
 import * as React from 'react';
 import tw from 'tailwind-rn';
@@ -11,19 +11,17 @@ const TodosList = ({ navigation }: any): JSX.Element => {
   const { state: { todos } } = useTodos();
 
   return (
-    <View style={tw('flex-1 p-3 bg-white justify-between')}>
+    <View style={tw('flex-1 relative p-3 bg-white')}>
       <FlatList
-        style={{ maxHeight: '80%' }}
         data={todos}
         keyExtractor={(item) => String(item.id)}
-        renderItem={({ item }) => (
-          <Todo {...item} />
-        )}
+        renderItem={({ item }) => <Todo {...item} />}
+        ListEmptyComponent={ <Text style={tw('text-3xl text-center text-gray-500')}>No tasks</Text> }
       />
-      <View style={tw('justify-center items-end mb-6')}>
+      <View style={tw('absolute bottom-0 right-0 mr-3 justify-center items-end mb-6')}>
         <Pressable
           style={tw('justify-center items-center')}
-          onPress={() => navigation.navigate('CreateTodo')}
+          onPress={() => navigation.navigate('Todo', { isNewTodo: true })}
         >
           <AntDesign name="pluscircle" size={56} style={tw('text-blue-500')} />
         </Pressable>
