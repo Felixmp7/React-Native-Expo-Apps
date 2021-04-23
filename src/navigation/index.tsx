af035550d4aac8bfcf8224548ab1dfb809d918cc
Felix Pacheco <felix@servisenior.com>
2021-04-22 20:29:32 -0400

    Hidde Header Navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,17 +2,15 @@ import * as React from 'react';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ColorSchemeName } from 'react-native';
-import NotFoundScreen from '../screens/NotFoundScreen';
 import HomeScreen from '../screens';
 import TodosNavigator from './TodosNavigator';
 
 const Stack = createStackNavigator();
 
 const RootNavigator = () => (
-  <Stack.Navigator initialRouteName="Home">
+  <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Home" options={{ title: 'Mobile Apps' }} component={HomeScreen} />
     <Stack.Screen name="Todos" component={TodosNavigator} />
-    <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
   </Stack.Navigator>
 );
 
