6218232f1b0e7485373155f8695b4567b8bebedc
Felix Pacheco <felix@servisenior.com>
2021-04-24 14:40:09 -0400

    Implement ChatNavigator

---
+++ src/chat-app/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+const Chat = createStackNavigator();
+
+const ChatNavigator = () => (
+    <Chat.Navigator initialRouteName="LoginScreen">
+        <Chat.Screen
+            name="LoginScreen"
+            options={{ title: 'Login' }}
+            component={LoginScreen}
+        />
+        <Chat.Screen
+            name="RegisterScreen"
+            options={{ title: 'Register' }}
+            component={RegisterScreen}
+        />
+    </Chat.Navigator>
+);
+
+export default ChatNavigator;
