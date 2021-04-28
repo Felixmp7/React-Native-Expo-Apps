11630f876984bcfed97ca6d6fe4afee46a8a5cc8
Felix Pacheco <felix@servisenior.com>
2021-04-27 21:02:56 -0400

    Implement login and handle listener to manage session active

--- src/chat-app/screens/LoginScreen.tsx
+++ src/chat-app/screens/LoginScreen.tsx
@@ -1,35 +1,56 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Alert, View } from 'react-native';
 import tw from 'tailwind-rn';
 import { Input, Button } from 'react-native-elements';
 import { MaterialIcons } from '@expo/vector-icons';
+import { auth } from '../services/firebase';
 
 const LoginScreen = ({ navigation }:any): JSX.Element => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    auth.signInWithEmailAndPassword(email, password)
+      .then(() => {
+        Alert.alert('Notification', 'Login successfully');
+        navigation.popToTop();
+        // ...
+      })
+      .catch((error: any) => {
+        Alert.alert('Notification', error.message);
+      });
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user: any) => {
+      if (user) {
+        navigation.replace('Chat');
+      } else {
+        navigation.canGoBack() && navigation.popToTop();
+        // No user is signed in.
+      }
+      return unsubscribe;
+    });
+  }, []);
 
   return (
     <View style={tw('flex-1 relative p-3 bg-white')}>
       <Input
         placeholder="Email"
         label="Email"
-        leftIcon={
-          <MaterialIcons name="email" size={24} color="black" />
-				}
+        leftIcon={<MaterialIcons name="email" size={24} color="black" />}
         value={email}
         onChangeText={(text) => setEmail(text)}
       />
       <Input
         placeholder="Password"
         label="Password"
-        leftIcon={
-          <MaterialIcons name="lock-outline" size={24} color="black" />
-				}
+        leftIcon={<MaterialIcons name="lock-outline" size={24} color="black" />}
         value={password}
         onChangeText={(text) => setPassword(text)}
         secureTextEntry
       />
-      <Button title="Log In" buttonStyle={tw('mb-4 w-1/2 self-center bg-indigo-600')} />
+      <Button title="Log In" onPress={handleLogin} buttonStyle={tw('mb-4 w-1/2 self-center bg-indigo-600')} />
       <Button title="Register" buttonStyle={tw('w-1/2 self-center bg-pink-600')} onPress={() => navigation.navigate('RegisterScreen')} />
     </View>
   );
