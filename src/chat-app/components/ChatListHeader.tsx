ca56700b3b5c85ffd000680081d03181c43e707a
Felix Pacheco <felix@servisenior.com>
2021-04-30 14:01:14 -0400

    Implement ChatListHeader

---
+++ src/chat-app/components/ChatListHeader.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Alert, Pressable } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { Entypo, AntDesign } from '@expo/vector-icons';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+import { auth } from '../services/firebase';
+import tw from 'tailwind-rn';
+import defaultImage from '../../constants/ProfileImages';
+
+const ChatListHeader = () => {
+    const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+
+    const signOut = async () => {
+        try {
+            await auth.signOut();
+            navigation.pop();
+        } catch (error) {
+            Alert.alert(error.message);
+        }
+    };
+
+
+    return (
+        <View style={tw('relative h-16 bg-indigo-900 justify-center items-center')}>
+            <Pressable onPress={signOut} style={tw('absolute ml-3 left-0')}>
+                <AntDesign name="leftcircleo" size={24} color="black" />
+            </Pressable>
+            <Avatar
+                rounded
+                size={42}
+                source={{ uri: auth?.currentUser?.photoURL || defaultImage }}
+            />
+            <Pressable onPress={() => navigation.navigate('NewChatScreen')} style={tw('absolute mr-3 right-0')}>
+                <Entypo name="new-message" size={24} color="white" />
+            </Pressable>
+        </View>
+    );
+};
+
+export default ChatListHeader;
