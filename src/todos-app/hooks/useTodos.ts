225fc8438381ed6b4345c73e061210eb4718bda9
Felix Pacheco <felix@servisenior.com>
2021-04-30 08:43:42 -0400

    Destructuring useContext

--- src/todos-app/hooks/useTodos.ts
+++ src/todos-app/hooks/useTodos.ts
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import { useContext } from 'react';
 import { TodosContext } from '../TodosContext';
 
 const useTodos = (): ContextProps => {
-    const context = React.useContext(TodosContext);
+    const context = useContext(TodosContext);
     if (context === undefined) {
         throw new Error('useTodos must be used within a TodosProvider');
     }
