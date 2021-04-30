43a4ba3a6ab44e11382d6bc898a413cdd90e04ea
Felix Pacheco <felix@servisenior.com>
2021-04-30 08:48:26 -0400

    Rename index

--- src/RootNavigator.tsx
+++ src/RootNavigator.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ColorSchemeName } from 'react-native';
-import HomeScreen from './index';
+import HomeScreen from '.';
 import TodosNavigator from './todos-app/navigation';
-import ChatNavigator from './chat-app';
+import ChatNavigator from './chat-app/ChatNavigator';
 
 const Stack = createStackNavigator();
 
