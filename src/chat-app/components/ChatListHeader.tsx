185be5a3f32d12f215ed4cdf010c92743881ad95
Felix Pacheco <devfex.19@gmail.com>
2021-05-05 19:51:43 -0400

    Fix styles

--- src/chat-app/components/ChatListHeader.tsx
+++ src/chat-app/components/ChatListHeader.tsx
@@ -7,7 +7,6 @@ import { Entypo, AntDesign } from '@expo/vector-icons';
 import { ParamListBase, useNavigation } from '@react-navigation/native';
 import { auth } from '../services/firebase';
 import tw from 'tailwind-rn';
-import defaultImage from '../../constants/ProfileImages';
 
 const ChatListHeader = () => {
     const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
@@ -23,17 +22,17 @@ const ChatListHeader = () => {
     };
 
     return (
-        <View style={tw('relative h-16 bg-indigo-900 justify-center items-center')}>
+        <View style={tw('relative h-16 justify-center items-center border-b border-gray-400')}>
             <Pressable onPress={signOut} style={tw('absolute ml-3 left-0')}>
-                <AntDesign name="leftcircleo" size={24} color="white" />
+                <AntDesign name="leftcircleo" size={24} color="black" />
             </Pressable>
             <Avatar
                 rounded
                 size={42}
-                source={{ uri: auth?.currentUser?.photoURL || defaultImage }}
+                source={{ uri: auth?.currentUser?.photoURL }}
             />
             <Pressable onPress={() => navigation.navigate('NewChatScreen')} style={tw('absolute mr-3 right-0')}>
-                <Entypo name="new-message" size={24} color="white" />
+                <Entypo name="new-message" size={24} color="black" />
             </Pressable>
         </View>
     );
